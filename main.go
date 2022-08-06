2c6ed4fb077f271d970eb105d4abeb4124688c1f
vintzvintz <git@vintz.fr>
2022-08-06 17:47:21 +0200

    ajoute un test sur main()

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"time"
+	"runtime"
+
 	"vintz.fr/nonogram/solver"
 	"vintz.fr/nonogram/tabjeu"
 )
 
+
 func main() {
-	tj := tabjeu.NewTabJeu(15, 40, 1003)
+	essai()
+}
+
+func essai() {
+	tj := tabjeu.NewTabJeu(15, 43, 1003)
 	tj.AfficheAvecComptes()
 	prob := tj.MakeProbleme()
-	solver.Bench(prob, false)
+	nbWorkers := runtime.NumCPU()
+	txt := fmt.Sprintf("%d workers", nbWorkers)
+
+	for iter :=0; iter<1; iter++ {
+		startTime := time.Now()
+		var nbSol int
+		for sol := range solver.SolveConcurrent(prob, nbWorkers, true) {
+			_ = sol
+			//sol.AfficheAvecComptes()
+			//fmt.Println(sol)
+			nbSol++
+		}
+		duree := time.Since(startTime)
+		fmt.Printf("%s : %d solutions trouvées en %v\n", txt, nbSol, duree)
+	}
 }
