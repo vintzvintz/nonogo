95d5ce3d0308ecdcbba51c6f3d70c1c12487e82b
vintzvintz <git@vintz.fr>
2022-08-07 18:57:27 +0200

    Modifie l'API du WorkerPool

--- solver/workers.go
+++ solver/workers.go
@@ -1,31 +1,24 @@
 package solver
 
 import (
+	_ "fmt"
 	"sync"
 )
 
-
 type TaskList []func()
 
-// WorkerPool est un pool de workers qui executent des taches func() 
+// WorkerPool est un pool de workers qui executent des taches func()
 // les taches sont des coroutines sans parametres ni valeurs de retour
 type WorkerPool struct {
 	ch chan func()     // queue d'entree du worker pool
 	wg *sync.WaitGroup // attend la fin de la recursion
 }
 
-
-// NewWorkQueue initialise et démarre un pool de nbWorkers workers 
-func NewWorkQueue( nbWorkers int) (wp *WorkerPool) {
-
-	if nbWorkers <= 0 {
-		return nil
-	}
+// NewWorkerPool initialise et démarre un pool de nbWorkers workers
+func NewWorkerPool(nbWorkers int) (wp *WorkerPool) {
 
 	wp = new(WorkerPool)
 	wp.wg = new(sync.WaitGroup)
-	// recursion monothread classique si workQueue.ch = nil
-
 	wp.ch = make(chan func())
 
 	// lance les workers
@@ -40,20 +33,28 @@ func NewWorkQueue( nbWorkers int) (wp *WorkerPool) {
 	return wp
 }
 
-// AddTasks ajoute des taches à traiter par les workers
-func (wp* WorkerPool) AddTasks( tasks TaskList ) {
-	// envoi des avec une goroutine
-	// les taches sont "consommées" à travers le chan par les workers
-	wp.wg.Add(len(tasks))
-	go func() {
-		for _, t := range tasks {
-			wp.ch <- t
-		}
-	}()
+// TryExec execute la tâche avec un worker disponible
+// si aucun worker n'est disponible, on renvoie false
+func (wp *WorkerPool) TryExec(task func()) (accepted bool) {
+	wp.wg.Add(1)
+	select {
+	case wp.ch <- task: // execution avec un worker disponible
+		accepted = true 
+		// wg.Done() appelé par le worker
+	default:
+		accepted = false
+		wp.wg.Done()
+	}
+	return accepted 
+}
+
+func (wp *WorkerPool) Exec(task func())  {
+	wp.wg.Add(1)
+	wp.ch <- task	
 }
 
 
 // Wait() bloque jusqu'au traitement de la dernière tâche
-func (wp* WorkerPool) Wait() {
+func (wp *WorkerPool) Wait() {
 	wp.wg.Wait()
 }
--- solver/workers_test.go
+++ solver/workers_test.go
@@ -8,72 +8,131 @@ import(
 )
 
 
-// makeTasks crée une liste de nb tâches 
-// chaque tâche renvoie un entier aléatoire lorsqu'elle est executée
-// somme est la somme des entiers qui seront ecrits par les tâches
-func makeTestTasks(t *testing.T, nb int, outC chan int ) (tl TaskList, somme int)  {
-
-	tl = make( TaskList, nb)
-	//var tmpVal int
-	dbg := make( []int, nb)   // affiche les valeurs attendues
-	for i:=0;i<nb;i++ {
-
-		val := rand.Intn(1000)
-		task := func() {
-			outC <- val
-		}
-		tl[i] = task
-		somme += val
-		dbg[i] = val
+func TestExec(t *testing.T) {
+	rand.Seed(time.Hour.Nanoseconds())
+	nbWorkers := []int{ 1, 2, runtime.NumCPU(), 100 }
+	// attention, deadlock avec 0 workers
+	for _,n := range nbWorkers {
+		testExec(t, n, 1000, 420 )
 	}
-	return tl, somme
 }
 
-func testWorkers(t *testing.T, nbWorkers, nbListes, nbTasks int) {
+
+func TestTryExec(t *testing.T) {
+	rand.Seed(time.Hour.Nanoseconds())
+	nbWorkers := []int{ 0, 1, 2, runtime.NumCPU(), 100 }
+	// attention, execution synchrone avec 0 workers 
+	for _,n := range nbWorkers {
+		testTryExec(t, n, 1000, 421 )
+	}
+}
+
+
+func testTryExec(t *testing.T, nbWorkers, nbTasks, sizeTask int) {
 
 	valuesC := make(chan int)      // reçoit les valeurs produites par les tâches
-	testOkC := make(chan bool)    // pour attendre la fin du test avant de renvoyer le resultat
+	//testOkC := make(chan bool)    // pour attendre la fin du test avant de renvoyer le resultat
 
-	var attendu int
+	wp := NewWorkerPool( nbWorkers )
+	tl, wantSum := prepareTasks(t, nbTasks, sizeTask, valuesC )
 
-	wp := NewWorkQueue( nbWorkers )
+	// lance la vérification des résultats (goroutine)
+	okC := checkInts(t, valuesC, wantSum, sizeTask*nbTasks)
 
 	// envoie des listes de taches au workerpool
-	for nList:=0; nList<nbListes; nList++ {
-		tl, somme := makeTestTasks(t, nbTasks, valuesC)
-		wp.AddTasks(tl)
-		attendu += somme
-
+	var nbFail int
+	for _, task := range tl {
+		accepted := wp.TryExec(task)    // non bloquant
+		if( !accepted ) {
+			// en cas d'echec de TryExec, on execute la tache de façon synchrone
+			nbFail++
+			task()
+		}
 	}
 
-	// additionne les valeurs reçues et compare avec le résutat attendu 
-	go func() {
-		var resultat int
-		for v := range valuesC {
-			//t.Log( "Reçoit", v)
-			resultat += v
-		}
-		ok := resultat==attendu
-		if !ok {
-			t.Errorf("WorkerPool ne renvoie pas le résulat attendu. (got %d, want %d)\n",resultat, attendu)
-		}
-		testOkC <- ok
-	}()
-
+	t.Logf("TryExec() avec %d workers / %d taches. (%d refus du worker pool)", nbWorkers, nbTasks, nbFail)
 	wp.Wait()
-	close(valuesC)
+	close(valuesC)   // provoque la fin de checkInts()
 
-	// attend la fin du test pour terminer
-	<-testOkC
+	// attend le signal de fin de checkInts()
+	<-okC 
 }
 
 
-func TestWorkerPool(t *testing.T) {
-	rand.Seed(time.Hour.Nanoseconds())
-	nbWorkers := []int{1, 2, runtime.NumCPU(), 100 }
-	//nbWorkers = []int{1}
-	for _,n := range nbWorkers {
-		t.Logf("WorkerPool test %d workers", n)
-		testWorkers(t, n, 500, 500)
+func testExec(t *testing.T, nbWorkers, nbTasks, sizeTask int) {
+
+	valuesC := make(chan int)      // reçoit les valeurs produites par les tâches
+	//testOkC := make(chan bool)    // pour attendre la fin du test avant de renvoyer le resultat
+
+	wp := NewWorkerPool( nbWorkers )
+	tl, wantSum := prepareTasks(t, nbTasks, sizeTask, valuesC )
+
+	// lance la vérification des résultats (goroutine)
+	okC := checkInts(t, valuesC, wantSum, sizeTask*nbTasks)
+
+	// envoie des listes de taches au workerpool
+	for _, task := range tl {
+		wp.Exec(task)    // bloque tant qu'il n'y a pas de worker dispo
 	}
+	t.Logf("TryExec() avec %d workers / %d taches", nbWorkers, nbTasks)
+	wp.Wait()
+	close(valuesC)   // provoque la fin de checkInts()
+
+	// attend le signal de fin de checkInts()
+	<-okC 
+}
+
+
+func checkInts ( t *testing.T, in chan int, wantSum, wantNb int ) (okC chan bool) {
+	okC = make(chan bool)
+	var gotSum, gotNb int
+	go func() {
+		for v := range in {
+			gotSum += v
+			gotNb += 1
+		}
+		ok := (gotSum==wantSum) && (gotNb==wantNb)
+		if !ok {
+			t.Errorf("WorkerPool ne renvoie pas le résulat attendu. (gotSum=%d/gotNb=%d, wantSum=%d/wantNb=%d)\n",gotSum, gotNb, wantSum, wantNb)
+		}
+		okC <- ok
+		close(okC)
+	}()
+	return okC
+}
+
+func prepareTasks(t *testing.T, nbTask, sizeTask int, valuesC chan int) (tl []func(), attendu int) {
+	
+	// prepare une liste de taches
+	tl = make ( []func(), nbTask )	
+	for i := range tl {
+		task, somme := makeTestTask( t, sizeTask, valuesC)
+		tl[i] = task
+		attendu += somme
+	}
+	return tl, attendu
+}
+
+
+// makeTasks crée une liste de nb tâches 
+// chaque tâche renvoie des entiers aléatoires dans outC lorsqu'elle est executée
+// somme est la somme des entiers qui seront ecrits par les tâches
+func makeTestTask( t *testing.T, nb int, outC chan int ) (task func(), somme int)  {
+
+	// prepare une liste de nombres
+	vals := make( []int, nb)
+	for i := range vals {
+		vals[i] = rand.Intn(3000)
+		somme += vals[i]
+	}
+
+	task = func() {
+		for _, v := range vals {
+			//if rand.Intn(10000)!=0 {   // pour déclencher des erreurs aleatoires
+				outC <- v 
+			//}
+		}
+	}
+
+	return task, somme
 }
